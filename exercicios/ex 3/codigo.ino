1e93fc2a181a30358c958d28db356a0dff62a300
itrolucas <99336807+itrolucas@users.noreply.github.com>
2022-03-23 20:57:27 -0300

    Create codigo.ino

---
+++ exercicios/ex 3/codigo.ino
@@ -0,0 +1,22 @@
+void setup()
+{
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5,OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(1, HIGH);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(5, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(1, LOW);  
+}
